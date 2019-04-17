736f7beb80ec0eebe661b41aafdd6fcf0474a8b9
Cord Slatton-Valle <cord.slatton-valle@inseego.com>
2019-04-17 08:11:09 -0700

    update for querySnapshot change

--- src/store.js
+++ src/store.js
@@ -18,16 +18,18 @@ fb.auth.onAuthStateChanged(user => {
     fb.postsCollection.orderBy('createdOn', 'desc').onSnapshot(querySnapshot => {
       // check if created by currentUser
       let createdByCurrentUser
+      let docChanges = querySnapshot.docChanges();
       if (querySnapshot.docs.length) {
-        createdByCurrentUser = store.state.currentUser.uid == querySnapshot.docChanges[0].doc.data().userId ? true : false
+        createdByCurrentUser = store.state.currentUser.uid == docChanges[0].doc.data().userId ? true : false
       }
 
       // add new posts to hiddenPosts array after initial load
-      if (querySnapshot.docChanges.length !== querySnapshot.docs.length
-          && querySnapshot.docChanges[0].type == 'added' && !createdByCurrentUser) {
+      docChanges = querySnapshot.docChanges();
+      if (docChanges.length !== querySnapshot.docs.length
+          && docChanges[0].type == 'added' && !createdByCurrentUser) {
 
-        let post = querySnapshot.docChanges[0].doc.data()
-        post.id = querySnapshot.docChanges[0].doc.id
+        let post = docChanges[0].doc.data()
+        post.id = docChanges[0].doc.id
 
         store.commit('setHiddenPosts', post)
       } else {
